b19a9c1bae3d57573c94023618b52434f058fdf7
Yita Chiang <ccpz@ytchiang.net>
2015-03-31 19:10:12 +0800

    initial commit

---
+++ Mod_Lang_CHT/Mod_Lang_CHT.cs
@@ -0,0 +1,77 @@
+﻿using ICities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColossalFramework.Plugins;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Mod_Lang_CHT
+{
+    public class Mod_Lang_CHT : IUserMod
+    {
+        private string locale_name = "zh-tw";
+
+        public string Name
+        {
+            get {
+                try
+                {
+                    Assembly asm = Assembly.GetExecutingAssembly();
+                    Stream st = asm.GetManifestResourceStream(asm.GetName().Name+"."+locale_name+".locale");
+
+#if (DEBUG)
+                        DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("File size: {0}", st.Length));
+#endif
+                    FileStream dst = File.OpenWrite("Files\\Locale\\"+locale_name+".locale");
+
+                    byte[] buffer = new byte[8 * 1024];
+                    int len=0;
+                    while ((len = st.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        dst.Write(buffer, 0, len);
+                    }
+                    dst.Close();
+                    st.Close();
+
+#if (DEBUG)
+                    DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("File write to: {0}", Path.GetFullPath(dst.Name)));
+                    DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("Current Language: {0}", ColossalFramework.Globalization.LocaleManager.defaultLanguage));
+#endif
+                    ColossalFramework.Globalization.LocaleManager.ForceReload();
+
+                    string[] locales = ColossalFramework.Globalization.LocaleManager.instance.supportedLocaleIDs;
+                    for (int i = 0; i < locales.Length; i++)
+                    {
+#if (DEBUG)
+                            DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("Locale index: {0}, ID: {1}", i, locales[i]));
+#endif
+                        if (locales[i].Equals(locale_name))
+                        {
+#if (DEBUG)
+                                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("Find locale {0} at index: {1}", locale_name, i));
+#endif
+                            ColossalFramework.Globalization.LocaleManager.instance.LoadLocaleByIndex(i);
+                        }
+                    }
+#if (DEBUG)
+                        DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("Post Setting Language: {0}", ColossalFramework.Globalization.LocaleManager.defaultLanguage));
+#endif
+                }
+                catch (Exception e)
+                {
+                    DebugOutputPanel.AddMessage(PluginManager.MessageType.Error, e.ToString());
+                }
+                return "Chinese Traditional localization mod"; 
+            }
+        }
+
+        public string Description
+        {
+            get { return "Here is where I define my mod"; }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Cities Skylines Mod_Lang_CHT
+
+
+TODO: Write a project description
+
+## Installation
+
+TODO: Describe the installation process
+
+## Usage
+
+TODO: Write usage instructions
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+TODO: Write history
+
+## Credits
+
+TODO: Write credits
+
+## License
+
+Apache License 2.0
