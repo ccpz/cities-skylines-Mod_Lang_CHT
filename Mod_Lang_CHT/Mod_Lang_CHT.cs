620d63aded29dd9009c96d566fc094fa8999e4af
Yita Chiang <ccpz@ytchiang.net>
2015-04-01 21:48:28 +0800

    Try UIButton code

--- Mod_Lang_CHT/Mod_Lang_CHT.cs
+++ Mod_Lang_CHT/Mod_Lang_CHT.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using ColossalFramework.Plugins;
 using System.Reflection;
 using System.Diagnostics;
+using ColossalFramework.UI;
+using UnityEngine;
 
 namespace Mod_Lang_CHT
 {
@@ -20,6 +22,26 @@ namespace Mod_Lang_CHT
             get {
                 try
                 {
+                    // the folllowing UI code is refering: https://github.com/elboletaire/cities-skylines-destroy-everything/blob/master/Source/Button.cs
+
+                    var uiView = UIView.GetAView();
+                    UIButton button = (UIButton)uiView.AddUIComponent(typeof(ColossalFramework.UI.UIButton));
+                    button.text = "繁體中文化說明";
+                    button.width = 150;
+                    button.height = 30;
+                    // Style the button to look like a menu button.
+                    button.normalBgSprite = "ButtonMenu";
+                    button.disabledBgSprite = "ButtonMenuDisabled";
+                    button.hoveredBgSprite = "ButtonMenuHovered";
+                    button.focusedBgSprite = "ButtonMenuFocused";
+                    button.pressedBgSprite = "ButtonMenuPressed";
+                    button.textColor = new Color32(255, 255, 255, 255);
+                    button.disabledTextColor = new Color32(7, 7, 7, 255);
+                    button.hoveredTextColor = new Color32(7, 132, 255, 255);
+                    button.focusedTextColor = new Color32(255, 255, 255, 255);
+                    button.pressedTextColor = new Color32(30, 30, 44, 255);
+                    button.transformPosition = new Vector3(1.3f, -0.85f);
+
                     Assembly asm = Assembly.GetExecutingAssembly();
                     Stream st = asm.GetManifestResourceStream(asm.GetName().Name+"."+locale_name+".locale");
 
