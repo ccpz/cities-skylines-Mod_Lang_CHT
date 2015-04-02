cc64b0522e3011a61ffec432ba07e15ebf61e9ce
江易達 <ccpz@ytchiang.net>
2015-04-02 15:25:19 +0800

    use different locale path under different OS

--- Mod_Lang_CHT/Mod_Lang_CHT.cs
+++ Mod_Lang_CHT/Mod_Lang_CHT.cs
@@ -15,6 +15,37 @@ namespace Mod_Lang_CHT
     {
         private string locale_name = "zh-tw";
 
+        //the following OS detect code is referring http://stackoverflow.com/questions/10138040/how-to-detect-properly-windows-linux-mac-operating-systems
+        public enum Platform
+        {
+            Windows,
+            Linux,
+            Mac
+        }
+
+        public static Platform RunningPlatform()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    // Well, there are chances MacOSX is reported as Unix instead of MacOSX.
+                    // Instead of platform check, we'll do a feature checks (Mac specific root folders)
+                    if (Directory.Exists("/Applications")
+                        & Directory.Exists("/System")
+                        & Directory.Exists("/Users")
+                        & Directory.Exists("/Volumes"))
+                        return Platform.Mac;
+                    else
+                        return Platform.Linux;
+
+                case PlatformID.MacOSX:
+                    return Platform.Mac;
+
+                default:
+                    return Platform.Windows;
+            }
+        }
+
         public string Name
         {
             get {
@@ -26,7 +57,24 @@ namespace Mod_Lang_CHT
 #if (DEBUG)
                         DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("File size: {0}", st.Length));
 #endif
-                    FileStream dst = File.OpenWrite("Files\\Locale\\"+locale_name+".locale");
+                    String dst_path = "";
+#if (DEBUG)
+                    DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("OS Type: {0}", RunningPlatform().ToString()));
+#endif
+                    switch (RunningPlatform())
+                    {
+                        case Platform.Windows:
+                            dst_path = "Files\\Locale\\" + locale_name + ".locale";
+                            break;
+                        case Platform.Mac:
+                            dst_path = "Cities.app/Contents/Resources/Files/Locale/" + locale_name + ".locale";
+                            break;
+                        case Platform.Linux:
+                            //TODO: find locale path under linux
+                            break;
+                    }
+
+                    FileStream dst = File.OpenWrite(dst_path);
 
                     byte[] buffer = new byte[8 * 1024];
                     int len=0;
